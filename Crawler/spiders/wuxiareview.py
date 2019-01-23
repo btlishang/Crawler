1b143a123d89724691897f9050ec5c26f7a1a063
lishang <lis@longbei.com>
2019-01-23 15:33:23 +0800

    修改网址

--- Crawler/spiders/wuxiareview.py
+++ Crawler/spiders/wuxiareview.py
@@ -7,12 +7,14 @@ from Crawler.items import CrawlerItem
 class WuxiareviewSpider(scrapy.Spider):
     name = 'wuxiareview'
     allowed_domains = ['www.wuxiareview.com']
-    start_urls = ['https://www.wuxiareview.com/category/gzmdzst/',
-                  'https://www.wuxiareview.com/category/xuanchu/',
+    start_urls = [
+                    # 'https://www.wuxiareview.com/category/gzmdzst/',
+                  # 'https://www.wuxiareview.com/category/xuanchu/',
                   'https://www.wuxiareview.com/category/xccsl/',
-                  'https://www.wuxiareview.com/category/daidai/',
-                  'https://www.wuxiareview.com/category/sand/',
-                  'https://www.wuxiareview.com/category/news/',]
+                  # 'https://www.wuxiareview.com/category/daidai/',
+                  # 'https://www.wuxiareview.com/category/sand/',
+                  # 'https://www.wuxiareview.com/category/news/',
+    ]
 
 
     def parse(self, response):
@@ -35,6 +37,7 @@ class WuxiareviewSpider(scrapy.Spider):
         item['datetime'] = article.css('.article-meta .item::text').extract_first()
         item['category'] = article.css('.article-meta .item > a::text').extract_first()
         item['title'] = article.css('.article-title > a::text').extract_first()
-        item['info'] = article.css('.article-content > p::text').extract()
+        info = article.css('.article-content > p::text').extract()
+        item['info'] = ''.join(info)
 
         yield item
